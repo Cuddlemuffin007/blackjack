e195f22ccdea1f92eab4bd19eb45e7e575eeacb6
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-09 15:19:17 -0500

    Initial commit

---
+++ blackjack.py
@@ -0,0 +1,54 @@
+# tiy blackjack oop project
+import random
+
+
+class Card:
+
+    SUITS = ('H', 'S', 'C', 'D')
+    RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return "{}{}".format(self.rank, self.suit)
+
+    def get_suit(self):
+        return self.suit
+
+    def get_rank(self):
+        return self.rank
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = [Card(suit, rank) for suit in Card.SUITS for rank in Card.RANKS]
+
+    def __str__(self):
+        deck = ""
+        for card in self.deck:
+            deck += (str(card) + " ")
+        return "Deck contains: " + deck
+
+    def shuffle(self):
+        return random.shuffle(self.deck)
+
+    def deal_card(self):
+        return self.deck.pop()
+
+
+class Hand:
+
+    def __init__(self):
+        self.hand = []
+
+    def __str__(self):
+        hand = ""
+        for card in self.hand:
+            hand += (str(card) + " ")
+        return "Hand contains: " + hand
+
+    def add_card(self, card):
+        self.hand.append(card)
